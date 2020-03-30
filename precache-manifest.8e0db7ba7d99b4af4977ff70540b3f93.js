3f635014ab061ba3e35405f28a06c92e63626293
Rizal Mifta <rizalmifta79@gmail.com>
2020-03-30 19:47:41 +0700

    Updates

---
+++ precache-manifest.8e0db7ba7d99b4af4977ff70540b3f93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b06b4e40b3469119833acf4a7dd248a",
+    "url": "/film/index.html"
+  },
+  {
+    "revision": "058f4bc4a1b50b20a2db",
+    "url": "/film/static/css/main.be3dfc19.chunk.css"
+  },
+  {
+    "revision": "903bcb89e21533789417",
+    "url": "/film/static/js/2.9cab3440.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/film/static/js/2.9cab3440.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "058f4bc4a1b50b20a2db",
+    "url": "/film/static/js/main.53810034.chunk.js"
+  },
+  {
+    "revision": "a44dc00ae89993b172d5",
+    "url": "/film/static/js/runtime-main.3717a3b1.js"
+  }
+]);
